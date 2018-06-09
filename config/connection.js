2a13bd3b41b586e703179773a2c3c760a196be6f
Celso Oliveira <celsooliveira@celsos-air.lan>
2018-06-08 21:48:31 -0700

    add jawsDB and heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 var mysql = require("mysql");
 // Inside the connection.js file, we setup the code to connect Node to MySQL.
 
-var connection = mysql.createConnection({
+var connection
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+
+
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "drinks_db"
 });
-
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
